94bb7287d4a37cb14aafccba1725f40284584690
Lody Borgers <l.borgers@taf.nl>
2018-02-07 17:08:54 +0100

    Created base workflow with css-next

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import './css/base.css';
+import './css/brand-1.css';
+
+// if (process.env.brand2) {
+//   import './css/brand-2.css';
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,17 @@ const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const baseStyle = new ExtractTextPlugin('styles/base.css');
+const brand1Style = new ExtractTextPlugin('styles/brand1.css');
+const brand2Style = new ExtractTextPlugin('styles/brand2.css');
+
 
 module.exports = {
-  entry: 'src/app.js',
+  entry: {
+    'app': path.join(__dirname, 'src', 'app.js'),
+  },
 
   devtool: 'inline-source-map',
 
@@ -16,8 +24,26 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
+        test: path.join(__dirname, 'src', 'css', 'base.css'),
+        use: baseStyle.extract({
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader'
+          ]
+        })
+      },
+      {
+        test: path.join(__dirname, 'src', 'css', 'brand-1.css'),
+        use: brand1Style.extract({
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader'
+          ]
+        })
+      },
+      {
+        test: path.join(__dirname, 'src', 'css', 'brand-2.css'),
+        use: brand2Style.extract({
           use: [
             { loader: 'css-loader', options: { importLoaders: 1 } },
             'postcss-loader'
@@ -28,7 +54,11 @@ module.exports = {
   },
 
   plugins: [
-    new ExtractTextPlugin('[name].css'),
+    new CleanWebpackPlugin('dist'),
+    // new ExtractTextPlugin('css/[name].css'),
+    baseStyle,
+    brand1Style,
+    brand2Style,
     new HtmlWebpackPlugin({
       title: 'TAF Styleguide experiment with Webpack and PostCSS'
     })
@@ -36,6 +66,6 @@ module.exports = {
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'app.[chunkHash].js'
+    filename: '[name].js'
   }
 };
